f77f64cd66b0fd0402d4ee15a48d4198ceb71d22
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-04-13 15:56:00 +0200

    improved routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
 import Store from "../store";
 import { createContext } from "react";
+import { useEffect } from "react";
 
 const store = new Store();
-export const Context = createContext<Store>( store );
+
+export const Context = createContext<Store>(store);
+const existingRoutes = [
+  "/intro",
+  "/registration",
+  "/camera-frame",
+  "/certificate",
+];
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!existingRoutes.includes(Router.pathname)) {
+      Router.push("/intro");
+    }
+  }, []);
   return (
     <Context.Provider value={store}>
       <Component {...pageProps} />
