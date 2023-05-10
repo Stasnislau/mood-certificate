d99a5722582d0dc8c73a630d6b8f0bf91933275d
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-05-10 23:41:58 +0200

    changed redirection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,11 @@ import { useEffect } from "react";
 const store = new Store();
 
 export const Context = createContext<Store>(store);
-const existingRoutes = [
-  "/",
-  "/registration",
-  "/camera-frame",
-  "/certificate",
-];
+const existingRoutes = ["/", "/registration", "/camera-frame", "/certificate"];
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (!existingRoutes.includes(Router.pathname)) {
-      Router.push("/intro");
+      Router.push("/");
     }
   }, []);
   return (
